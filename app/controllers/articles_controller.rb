42154704550bf8369c98f8ec5e8b7c4b6eca3f35
Shun Sugai <sugaishun@gmail.com>
2014-05-22 00:24:07 +0900

    Handle nested uri

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @articles = @category.articles.all
+    elsif params[:blog_id]
+      @blog = Blog.find(params[:blog_id])
+      @articles = @blog.articles.all
+    else
+      @articles = Article.all
+    end
   end
 end
