e6d6dccfdc86fcb47756127ddc262900fe5c595c
Shun Sugai <sugaishun@gmail.com>
2014-05-26 23:43:14 +0900

    Add articles request spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -24,4 +24,52 @@ describe 'Articles' do
       expect(json['entries'][0]['link']).to eq('http://example.com/this_is_test_entry_001')
     end
   end
+
+  describe 'GET /categories/:category_id/article.json' do
+    before(:each) do
+      @category = FactoryGirl.create(:category)
+      @blog     = FactoryGirl.create(:blog, category: @category)
+      @article  = FactoryGirl.create(:article, blog: @blog)
+      get "/categories/#{@category.id}/articles.json", format: :json
+    end
+
+    it 'response 200 OK' do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it 'item have title' do
+      json = JSON.parse(response.body)
+      expect(json['entries'][0]['title']).to eq('This is test entry')
+    end
+
+    it 'item have link' do
+      json = JSON.parse(response.body)
+      expect(json['entries'][0]['link']).to eq('http://example.com/this_is_test_entry_001')
+    end    
+  end
+
+  describe 'GET /blogs/:blog_id/articles.json' do
+    before(:each) do
+      @category = FactoryGirl.create(:category)
+      @blog     = FactoryGirl.create(:blog, category: @category)
+      @article  = FactoryGirl.create(:article, blog: @blog)
+      get "/blogs/#{@blog.id}/articles.json", format: :json
+    end
+
+    it 'response 200 OK' do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it 'item have title' do
+      json = JSON.parse(response.body)
+      expect(json['entries'][0]['title']).to eq('This is test entry')
+    end
+
+    it 'item have link' do
+      json = JSON.parse(response.body)
+      expect(json['entries'][0]['link']).to eq('http://example.com/this_is_test_entry_001')
+    end
+  end
 end
