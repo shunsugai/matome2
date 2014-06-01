50a395e84a6aad64cf66224790c29cb1d54dbf7d
Shun Sugai <sugaishun@gmail.com>
2014-06-01 17:02:40 +0900

    Validates empty attributes.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ActiveRecord::Base
   belongs_to :category
   has_many   :articles
+
+  validates :name, presence: true
+  validates :url,  presence: true
+  validates :rss,  presence: true
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Blog do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blog) { FactoryGirl.create(:blog) }
+
+  specify 'valid object' do
+    expect(blog).to be_valid
+  end
+
+  %w(name url rss).each do |attribute|
+    specify "#{attribute} must not be empty" do
+      blog[attribute] = ''
+      expect(blog).not_to be_valid
+      expect(blog.errors[attribute]).to be_present
+    end
+  end
 end
