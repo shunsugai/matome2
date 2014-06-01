8a295fa734852f8494b15327c6938f54eefbb42a
Shun Sugai <sugaishun@gmail.com>
2014-06-01 17:56:31 +0900

    Article 'title' and 'url' must not be empty

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { FactoryGirl.create(:article) }
+
+  specify 'valid object' do
+    expect(article).to be_valid
+  end
+
+  %w(title url).each do |attribute|
+    specify "#{attribute} must not be empty" do
+      article[attribute] = ''
+      expect(article).not_to be_valid
+      expect(article.errors[attribute]).to be_present
+    end
+  end
+
 end
