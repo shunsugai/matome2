68717ae2c2f919a651ea1253182f211f63866cd4
Shun Sugai <sugaishun@gmail.com>
2014-06-14 00:30:52 +0900

    Add default scope

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,15 @@
 class Blog < ActiveRecord::Base
+  # relations
   belongs_to :category
   has_many   :articles
 
+  # validations
   validates :name, presence: true, length: { maximum: 50 }
   validates :url,  presence: true, length: { maximum: 2000 }
   validates :rss,  presence: true, length: { maximum: 2000 }
   validates :url, url: true, allow_blank: true
   validates :rss, url: true, allow_blank: true
+
+  # scopes
+  default_scope { where(show: true) }
 end
