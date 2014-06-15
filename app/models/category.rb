5e2dd8e8f8ea1b70913b30bd80dc837f8e624f48
Shun Sugai <sugaishun@gmail.com>
2014-06-15 21:41:33 +0900

    Displays categories which show flag is true

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ActiveRecord::Base
+  # relations
   has_many :blogs
   has_many :articles, :through => :blogs
 
+  # validations
   validates :name, presence: true, length: { maximum: 20 }
+
+  # scopes
+  default_scope { where(show: true) }
 end
